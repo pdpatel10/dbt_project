7899194302a8674285146e9ac11c34b68251733a
priya_training_cy@outlook.com <priya_training_cy@outlook.com>
2022-05-19 09:26:27 +0000

    Added staging, models and schema

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,17 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+select
+    id as customer_id,
+    first_name,
+    last_name
+
+from raw.jaffle_shop.customers
---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,10 @@
+
+-- Use the `ref` function to select from other models
+
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+
+from raw.jaffle_shop.orders
